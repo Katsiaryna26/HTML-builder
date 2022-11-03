5261b1d4c910905757af809a3051f64ad5781387
Katsiaryna26 <mary260188@mail.ru>
2022-11-03 12:00:02 +0300

    feat:create code to read file with application output in console

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const redstrem = fs.createReadStream('text.txt','utf-8');
+let data = '';
+redstrem.on('data',chunk=>data+=chunk);
+redstrem.on('end',()=>console.log('End',data));
+redstrem.on('error',error=>console.log('Error',error.message));
