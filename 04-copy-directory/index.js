921273f2f0c38b215296725ac0f62b451568243f
Katsiaryna26 <mary260188@mail.ru>
2022-11-06 23:32:08 +0300

    feat: create code to copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
 
+fs.mkdir(path.join(__dirname,'files-copy'),{recursive: true}, err=>{
+    if (err) throw err;
+})
+
+if (path.join(__dirname,'files-copy')) {
+    fs.readdir(path.join(__dirname,'files-copy'), (err, files)=>{
+        files.forEach(file=>{
+            fs.rm((path.join(__dirname,'files-copy',file)),err=>{
+                if (err) throw err;
+            })
+        })
+    })
+}
+
+fs.readdir(path.join(__dirname,'files'), (err, files)=>{
+    files.forEach(file=>{
+        fs.copyFile(path.join(__dirname,'files', file), path.join(__dirname,'files-copy',file), err=>{
+            if (err) throw err;
+        })
+    })
+})
